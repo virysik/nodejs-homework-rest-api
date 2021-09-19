45d9bb30a88516472bab920f6e53088643cb5ead
virysik <68701496+virysik@users.noreply.github.com>
2021-09-19 19:17:07 +0200

    added getAllContacts and getContactsByID controllers

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,12 +1,41 @@
 const express = require('express')
+
+const contactOperations = require('../../model/contacts')
+
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const contacts = await contactOperations.listContacts()
+    res.json({
+      status: 'success',
+      code: 200,
+      data: { result: contacts },
+    })
+  } catch (error) {
+    next(error)
+  }
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const { contactId } = req.params
+    const contact = await contactOperations.getContactById(contactId)
+
+    if (!contact) {
+      const error = new Error(`Contact with id:${contactId} was not found`)
+      error.status = 404
+      throw error
+    }
+
+    res.json({
+      status: 'success',
+      code: 200,
+      data: { result: contact },
+    })
+  } catch (error) {
+    next(error)
+  }
 })
 
 router.post('/', async (req, res, next) => {
