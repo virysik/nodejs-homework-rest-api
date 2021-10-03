0db22cc4a677204ab7cb0930719a15bdd7808262
virysik <68701496+virysik@users.noreply.github.com>
2021-10-03 20:55:28 +0200

    added authenticate middleware

--- routes/auth/users.js
+++ routes/auth/users.js
@@ -3,7 +3,11 @@ const express = require('express')
 const ctrl = require('../../controllers/auth')
 
 const { joiAuthSchema } = require('../../models/user')
-const { controllerWrapper, validation } = require('../../middlewares')
+const {
+  controllerWrapper,
+  validation,
+  authenticate,
+} = require('../../middlewares')
 
 const router = express.Router()
 
@@ -13,11 +17,7 @@ router.post(
   controllerWrapper(ctrl.signup),
 )
 router.post('/login', validation(joiAuthSchema), controllerWrapper(ctrl.login))
-router.get('/ logout', controllerWrapper(ctrl.logout))
-router.get(
-  '/current',
-  validation(joiAuthSchema),
-  controllerWrapper(ctrl.current),
-)
+router.get('/logout', authenticate, controllerWrapper(ctrl.logout))
+router.get('/current', authenticate, controllerWrapper(ctrl.current))
 
 module.exports = router
