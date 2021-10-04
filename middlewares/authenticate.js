19de21dc5a595894526ff8a26ec8dd79dad98e5e
virysik <68701496+virysik@users.noreply.github.com>
2021-10-04 11:50:50 +0200

    updated middleware authenticate

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -1,4 +1,5 @@
 const { User } = require('../models')
+const { Unauthorized } = require('http-errors')
 const jwt = require('jsonwebtoken')
 
 const { SECRET_KEY } = process.env
@@ -7,42 +8,26 @@ const authenticate = async (req, res, next) => {
   const { authorization } = req.headers
 
   if (!authorization) {
-    res.status(401).json({
-      status: 'error',
-      code: 401,
-      message: 'Not authorized',
-    })
+    throw new Unauthorized('Not authorized')
   }
 
   const [bearer, token] = authorization.split(' ')
 
   if (bearer !== 'Bearer') {
-    res.status(401).json({
-      status: 'error',
-      code: 401,
-      message: 'Not authorized',
-    })
+    throw new Unauthorized('Not authorized')
   }
 
   try {
     const { _id } = jwt.verify(token, SECRET_KEY)
     const user = await User.findById(_id)
     if (!user.token) {
-      res.status(401).json({
-        status: 'error',
-        code: 401,
-        message: 'Not authorized',
-      })
+      throw new Unauthorized('Not authorized')
     }
 
     req.user = user
     next()
   } catch (error) {
-    res.status(401).json({
-      status: 'error',
-      code: 401,
-      message: 'Not authorized',
-    })
+    throw new Unauthorized('Not authorized')
   }
 }
 
